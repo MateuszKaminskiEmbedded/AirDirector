7ed906ebe8be406b3874b7ef71b754c726a61b61
Mateusz Kaminski <kaminskimateusz70@gmail.com>
2023-11-26 16:44:00 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Mateusz
+ *
+ * Created on 26 listopada 2023, 16:40
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
